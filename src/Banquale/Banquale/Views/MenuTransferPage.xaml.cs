9c167b978a21dd43235d44633b49be817a26c31a
anperederi <antoine.perederii@etu.uca.fr>
2023-04-19 14:14:04 +0200

    ajout des différentes vues pour les transactions (demande et envoie d'argent et RIB)

---
+++ src/Banquale/Banquale/Views/MenuTransferPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Banquale.Views;
+
+public partial class MenuTransferPage : ContentPage
+{
+	public MenuTransferPage()
+	{
+        InitializeComponent();
+	}
+
+    void ContentPage_NavigatedTo(System.Object sender, Microsoft.Maui.Controls.NavigatedToEventArgs e)
+    {
+    }
+}
