ba9fc6d58c04a2bbed16492f973390a6f1320c71
anperederi <antoine.perederii@etu.uca.fr>
2023-05-03 11:47:47 +0200

    ajout d'un essaie de DataBinding sur la page NewPage1

--- src/Banquale/Banquale/Model/Personne.cs
+++ src/Banquale/Banquale/Model/Personne.cs
@@ -4,16 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Banquale.Model
 {
     public class Personne
     {
         public string Nom { get; private set; }
-
         public string Prenom { get; private set; }
-
         public int Id { get; private set; }
-
         public string Mdp { get; private set; }
 
         public Personne(string nom, string prenom, string mdp)
@@ -23,5 +21,7 @@ namespace Banquale.Model
             Id = 0;
             Mdp = mdp;
         }
+
     }
+
 }
