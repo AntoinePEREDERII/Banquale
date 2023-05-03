e79a1f802acb9d10f662b211bfb466c178939bd3
anperederi <antoine.perederii@etu.uca.fr>
2023-05-03 11:56:36 +0200

    modif de l'app Shell pour le dataBinding

--- src/Banquale/Banquale/App.xaml.cs
+++ src/Banquale/Banquale/App.xaml.cs
@@ -6,11 +6,17 @@ public partial class App : Application
 
 	public Manager MyManager { get; private set; } = new Manager();
 
+	
+
 	public App()
 	{
-		InitializeComponent();
+
+        MyManager.Donnee();
+
+        InitializeComponent();
 
 		MainPage = new AppShell();
+
 	}
 }
 
