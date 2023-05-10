b8a8cf554eea0381f8c79cca50476b2e9adfa6d9
anperederi <antoine.perederii@etu.uca.fr>
2023-05-10 09:38:37 +0200

    Add DataContractPersistance

---
+++ src/Banquale/Banquale/DataContractPersistance/DataContractPers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Banquale.DataContractPersistance
+{
+	public class DataContractPers
+	{
+		public DataContractPers()
+		{
+
+			public string Filename { get; set; } = "PATH";
+
+			public string FilePath { get; set; } = "PATH";
+
+		}
+
+	public (List<Client>, List>Transactions>)
+	}
+}
+
