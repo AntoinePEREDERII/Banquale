31275eb0d974821cb780644040161b49cde44884
anperederi <antoine.perederii@etu.uca.fr>
2023-05-10 08:55:36 +0200

    Update Stub classes

---
+++ src/Banquale/Banquale/Stub/Stub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Banquale.Model;
+
+namespace Banquale.Stub
+{
+	public class Stub : IPersistanceManager
+	{
+
+        (List<Client>, List<Transactions>) IPersistanceManager.ChargeDonnee()
+        {
+            Client Client1 = new Client("Jacques", "Morice", "J'aimeLesFrites");
+            Client Client2 = new Client("Francis", "Begore", "J'aimeLes");
+            Client Client3 = new Client("Michel", "Boudout", "MonMdP");
+            Console.WriteLine(Client1);
+            List<Client> ListeClients = new List<Client>();
+            ListeClients.Add(Client1);
+            ListeClients.Add(Client2);
+            ListeClients.Add(Client3);
+            return ListeClients;
+        }
+
+        void IPersistanceManager.SauvegardeDonnee(List<Client> c, List<Transactions> t)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
+
