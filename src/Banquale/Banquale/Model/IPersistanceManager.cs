f3f000059e27c2e9b81c6eb1b2e8ecbb10a029d9
titouan.louvet <titouan.louvet@etu.uca.fr>
2023-05-10 08:40:01 +0200

    Added IPersistanceManager.cs

---
+++ src/Banquale/Banquale/Model/IPersistanceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banquale.Model
+{
+    interface IPersistanceManager
+    {
+        (List<Client>, List<Transactions>) ChargeDonnee();
+
+        void SauvegardeDonnee(List<Client> c, List<Transactions> t);
+    }
+}
