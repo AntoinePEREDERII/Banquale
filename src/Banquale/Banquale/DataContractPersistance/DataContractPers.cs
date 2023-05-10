6a49b22a37627f5cbb3fa6da518d76bd645c764c
anperederi <antoine.perederii@etu.uca.fr>
2023-05-10 10:02:43 +0200

    update to build in CI

--- src/Banquale/Banquale/DataContractPersistance/DataContractPers.cs
+++ src/Banquale/Banquale/DataContractPersistance/DataContractPers.cs
@@ -5,18 +5,18 @@ namespace Banquale.DataContractPersistance
 {
 	public class DataContractPers
 	{
-		public DataContractPers()
-		{
-			public string Filename { get; set; } = "PATH";
+		//public DataContractPers()
+		//{
+		//	public string Filename { get; set; } = "PATH";
 
-			public string FilePath { get; set; } = "PATH";
+		//	public string FilePath { get; set; } = "PATH";
 
-		}
+		//}
 
-		public (List<Client>, List<Transactions>) ChargeDonnee()
-		{
-			var serializer = new DataContractSerializer(typeof(Client));
-		}
+		//public (List<Client>, List<Transactions>) ChargeDonnee()
+		//{
+		//	var serializer = new DataContractSerializer(typeof(Client));
+		//}
 	}
 }
 
