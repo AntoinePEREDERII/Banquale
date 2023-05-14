95c0d7a3f8a98c051bbebd84925be727cae74d58
anperederi <antoine.perederii@etu.uca.fr>
2023-05-14 19:17:05 +0200

    deployment of TestUnit

---
+++ src/Banquale/TestUnit/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace TestUnit;
+
+public class UnitTest1// une classe = type de chose à tester
+{   // ex : class envoie argent
+    [Fact]
+    public void Test1() // test unitaire tester chaque petit truc et test intégration qui regarde l'intégration de ttes la classe
+    { 
+    }
+}
