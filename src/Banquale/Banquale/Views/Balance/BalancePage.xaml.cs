20c7b3000a866aed60111f0327f9d11ac95c11db
anperederi <antoine.perederii@etu.uca.fr>
2023-06-08 21:56:40 +0200

    update message list page

--- src/Banquale/Banquale/Views/Balance/BalancePage.xaml.cs
+++ src/Banquale/Banquale/Views/Balance/BalancePage.xaml.cs
@@ -11,13 +11,6 @@ public partial class BalancePage : ContentPage
 	{
 		InitializeComponent();
         BindingContext = Mgr.SelectedAccount;
-        if(Mgr.IsConsultant == true)
-        {
-            //Label lext = new Label { Text = "Hello" };
-            //Grid.Add(lext);
-            Image backArrow = new Image { Source = "{StaticResource AccountIcon}", HeightRequest = 100 };
-            Grid.Add(backArrow);
-        }
     }
 
     public async void Balance_Clicked(object sender, EventArgs e)
--- src/Banquale/Banquale/Views/Balance/BalanceView.xaml.cs
+++ src/Banquale/Banquale/Views/Balance/BalanceView.xaml.cs
@@ -23,27 +23,5 @@ public partial class BalanceView : ContentView
             Mgr.SelectedTransaction = selectedItem;
             await Navigation.PushModalAsync(new TransactionsPage());
         }
-
-        //if (string.IsNullOrWhiteSpace(idLabel.Text))
-        //{
-        //    //await DisplayAlert("Erreur", "Aucune transaction présente", "OK");
-        //    Debug.WriteLine("Erreur1");
-        //    return;
-        //}
-
-        //Transaction transaction = Mgr.SelectedAccount.TransactionsList.FirstOrDefault(u => u.Id == TransactionId);
-        //if (transaction == null)
-        //{
-        //    Debug.WriteLine("Erreur2");
-        //    //await DisplayAlert("Erreur", "La transaction n'éxiste pas !", "OK");
-        //    return;
-        //}
-
-        //Mgr.SelectedTransaction = transaction;
-
-        //Mgr.SelectedTransaction = Mgr.CustomersList[0].AccountsList[0].TransactionsList[0];
-
-
-        //await Navigation.PushModalAsync(new TransactionsPage());
     }
 }
