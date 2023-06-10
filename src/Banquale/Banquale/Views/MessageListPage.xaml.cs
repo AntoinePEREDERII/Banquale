832da57cba355dd04caa9ffd95ec528c60e54472
titouan.louvet <titouan.louvet@etu.uca.fr>
2023-06-10 15:47:50 +0200

    update messagePage

--- src/Banquale/Banquale/Views/MessageListPage.xaml.cs
+++ src/Banquale/Banquale/Views/MessageListPage.xaml.cs
@@ -13,6 +13,21 @@ public partial class MessageListPage : ContentPage
         Debug.WriteLine(Mgr.Consultant.MessagesList[0].Subject);
         BindingContext = Mgr;
         InitializeComponent();
-        
+    }
+
+    public async void MessageDelete(Object sender, EventArgs e)
+    {
+        var selectedItem = (sender as Grid)?.BindingContext as Message;
+
+        if (selectedItem != null)
+        {
+            bool rep = await DisplayAlert("Suppression", "Voulez vous supprimer ce message ?", "Oui", "Non");
+            Debug.WriteLine("Rep : " + rep);
+            if(rep)
+            {
+                Mgr.Consultant.MessagesList.Remove(selectedItem);
+                await Shell.Current.Navigation.PopAsync();
+            }
+        }
     }
 }
